202addbdaae38e4d8563b9f47ecc52aa67402a6a
zoeurk <zoeurk@gmail.com>
2021-11-24 19:50:53 +0100

    update

--- exemple/main.c
+++ exemple/main.c
@@ -6,9 +6,7 @@
 void cosinus(char *arg){
 	char pi[60], npi[60],*pi_, *npi_ ,*temp;
 	long double x;
-	//memset(npi, 0, 51);
 	sprintf(npi,"-%.56Lf", (long double)M_PI);
-	//memset(pi, 0, 51);
 	sprintf(pi,"%.56Lf", (long double)M_PI);
 	pi_ = multiplication(pi, "2");
 	npi_ = multiplication(npi, "-2");
@@ -33,13 +31,10 @@ void cosinus(char *arg){
 	free(pi_);
 	free(npi_);
 }
-//#define MIN 10000
-//#define Q_MIN "10000"
 void racine_carree(char *i, unsigned long int virgule, unsigned long int coefficient){
 	//printf("%s\n", i);
-	char *pi,*j, *pj,rc[60], arrondi[58], m[21], *eq = NULL;
+	char *pi,*j, *pj,rc[60], m[21];
 	long double r1;
-	//char t[2] = { 0, 0 };
 	unsigned long long int  max = coefficient;
 	if(equal(i, "0") < 0){
 		printf("ERREUR: Valeur: %s < 0\n", i);
@@ -48,57 +43,25 @@ void racine_carree(char *i, unsigned long int virgule, unsigned long int coeffic
 	memset(m,0,21);
 	sprintf(m,"%llu", max);
 	sprintf(rc, "%.56Lf", sqrtl(max));
-	memset(arrondi,'0',58);
-	arrondi[57] = 0;
-	arrondi[1] = '.';
-	arrondi[56] = '1';
 	//printf("%s\n", rc);
 	pi = multiplication(i,"1");
 	pj = multiplication("1","1");
 	while(equal(pi,m) > 0){
-		//printf("***\n");
-		//printf("*\n");
-		//printf("%s\n", rc);
 		j = pj;
 		pj = multiplication(rc, pj);
 		free(j);
 		j = pi;
-		/*pl = multiplication(j, j);
-		if(equal(i, pl) < 0)
-			break;*/
 		pi = division(j,m,virgule);
 		free(j);
 		j = pi;
-		//if(eq)
-		//	free(eq);
-		//eq = multiplication(j,j);
 	}
-	//if(eq)
-	//	free(eq);
 	j = pj;
 	r1 = strtold(pi, NULL);
 	r1 = sqrtl(r1);
 	sprintf(rc, "%.56Lf", r1);
 	j = multiplication(pj,rc);
-	//printf("==>%s\n", pj);
 	free(pj);
 	free(pi);
-	/*pj = multiplication(j,j);
-	printf("==>%s\n", pj);
-	free(pj);*/
-	/*if((pj = strchr(j, '.'))){
-		if(strlen(pj+1) > 56){
-			t[0] = *(pj+56);
-			if(atoi(t) >= 5){
-				pj = j;
-			j = addition(pj, arrondi);
-			free(pj);
-			pj = strchr(j, '.');
-			*(pj+57) = 0;
-		}else
-			*(pj+57) = 0;
-		}
-	}*/
 	//printf("%s\n", (char *)multiplication(j,j));
 	printf("Racine carree arbitraire pour \'%s\':%s\n" , i,j);
 	printf("Racine carree pour \'%s\':%.56Lf\n", i, sqrtl(strtold(i, NULL)));
