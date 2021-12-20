2558718834e06196e89de3097d5a2227fe515fb2
zoeurk <zoeurk@gmail.com>
2021-12-20 19:52:50 +0100

    update

--- exemple/main.c
+++ exemple/main.c
@@ -218,30 +218,33 @@ int main(int argc, char **argv){
 		free(r);
 	}
 	printf("++++++++++++++++++\n");
-	r = racine_carree(argv[1], atoi(argv[3]), 1);
-	check = multiplication(r, r);
-	printf("Racine carree arbitraire (arrondie) de \'%s\':%s\n",argv[1], r);
-	printf("Verification: %s\n", check);
-	free(r);
-	free(check);
-	r = racine_carree(argv[1], atoi(argv[3]), 0);
-	check = multiplication(r, r);
-	printf("Racine carree arbitraire de \'%s\':%s\n",argv[1], r);
-	printf("Verification: %s\n", check);
-	free(r);
-	free(check);
-	r = racine_carree(argv[2], atoi(argv[3]), 1);
-	check = multiplication(r, r);
-	printf("Racine carree (arrondie) de \'%s\':%s\n",argv[2], r);
-	printf("Verification: %s\n", check);
-	free(r);
-	free(check);
-	r = racine_carree(argv[2], atoi(argv[3]),0);
-	check = multiplication(r, r);
-	printf("Racine carree de \'%s\':%s\n",argv[2], r);
-	printf("Verification: %s\n", check);
-	free(r);
-	free(check);
+	if(equal(argv[1],"0") >= 0){
+		r = racine_carree(argv[1], atoi(argv[3]), 1);
+		check = multiplication(r, r);
+		printf("Racine carree arbitraire (arrondie) de \'%s\':%s\n",argv[1], r);
+		printf("Verification: %s\n", check);
+		free(r);
+		free(check);
+		r = racine_carree(argv[1], atoi(argv[3]), 0);
+		check = multiplication(r, r);
+		printf("Racine carree arbitraire de \'%s\':%s\n",argv[1], r);
+		printf("Verification: %s\n", check);
+		free(r);
+		free(check);
+	if(equal(argv[2],"0") >= 0){
+		r = racine_carree(argv[2], atoi(argv[3]), 1);
+		check = multiplication(r, r);
+		printf("Racine carree (arrondie) de \'%s\':%s\n",argv[2], r);
+		printf("Verification: %s\n", check);
+		free(r);
+		free(check);
+		r = racine_carree(argv[2], atoi(argv[3]),0);
+		check = multiplication(r, r);
+		printf("Racine carree de \'%s\':%s\n",argv[2], r);
+		printf("Verification: %s\n", check);
+		free(r);
+		free(check);
+	}
 	/*printf("%Lf\n", sqrtl(strtold(argv[1], NULL)));*/
 	return 0;
 }
