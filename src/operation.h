cec844706fb94d9f5fc2cc29ea171bea0174dacd
zoeurk <zoeurk@gmail.com>
2022-04-07 12:59:29 +0200

    update

--- src/operation.h
+++ src/operation.h
@@ -16,11 +16,9 @@
 
 #define NEG \
 	for(n1 = n1; *n1 == '-' || *n1 == '+'; n1++) \
-		if(*n1 == '-')\
-			neg1 = !neg1; \
+		neg1 = !neg1; \
 	for(n2 = n2; *n2 == '-' || *n2 == '+'; n2++) \
-		if(*n2 == '-') \
-			neg2 = !neg2;
+		neg2 = !neg2;
 
 #define NEG_TEST \
 	if(neg1 != neg2)\
@@ -46,14 +44,14 @@ while(*n2 == '0' && *(n2+1) != '.') \
 		*pbuf = buffer[ii]; \
 	} \
 	*buffer = '-';
-extern const unsigned long int BUFFER;
-int strtype(void *str);
-void *allocation(void **ptr, unsigned long int members, unsigned long int size);
+
+#define BUFFER 3
+void *allocation(void **ptr, size_t members, unsigned long int size);
 void *reallocation(void **ptr, unsigned long int size);
 int equal(void *num1, void *num2);
 void *addition(void *num1, void *num2);
 void *soustraction(void *num1, void *num2);
 void *multiplication(void *num1, void *num2);
-void *division(void *num1, void *num2, unsigned long int virgule, int approximation);
-void *modulo(void *num1, void *num2, unsigned long int virgule);
+void *division(void *num1, void *num2, unsigned long int virgule);
+void *modulo(void *num1, void *num2);
 #endif
